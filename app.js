0ee2a4cc4a488a3ac4eb2c9f159f764be98f655e
Aus Gomez <austrebertog@gmail.com>
2021-02-12 21:51:45 -0600

    init

---
+++ app.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const pdf = require('pdf-poppler');
+
+let file = 'file.pdf'
+
+let opts = {
+    format: 'jpeg',
+    out_dir: path.dirname(file),
+    out_prefix: 'file',
+    page: null
+}
+
+pdf.convert(file, opts)
+    .then(res => {
+        console.log('Successfully converted');
+    })
+    .catch(error => {
+        console.error(error);
+    })
